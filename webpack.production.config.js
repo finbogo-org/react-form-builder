02a1557b59bda7f22df04f14fa71d8541c8240b3
Agus Mathew <agusmathew@gmail.com>
2024-10-10 09:41:02 +0100

    update entry point in webpack config

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -2,7 +2,7 @@ var path = require("path");
 var webpack = require("webpack");
 
 module.exports = {
-  entry: "./src/index.jsx",
+  entry: "./app.js",
 
   output: {
     path: path.resolve("./dist"),
