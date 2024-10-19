209539d11cec1b0851d86a0162952616a77d4544
codelikesree@gmail.com <codelikesree@gmail.com>
2024-10-19 23:25:41 +0100

    fix/build-issue

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -2,39 +2,17 @@ var path = require("path");
 var webpack = require("webpack");
 
 module.exports = {
-  entry: "./src/index.jsx",
-
+  entry: "./app.js",
+  devtool: "source-map",
   output: {
-    path: path.resolve("./dist"),
+    path: path.resolve("./public"),
     filename: "app.js",
     library: "ReactFormBuilder",
     libraryTarget: "umd",
     umdNamedDefine: true,
   },
-
-  externals: {
-    //don't bundle the 'react' npm package with our bundle.js
-    //but get it from a global 'React' variable
-    react: {
-      commonjs: "react",
-      commonjs2: "react",
-      amd: "react",
-      root: "React",
-    },
-    "react-dom": {
-      commonjs: "react-dom",
-      commonjs2: "react-dom",
-      amd: "react-dom",
-      root: "ReactDOM",
-    },
-    // 'react-datepicker': 'react-datepicker',
-    // 'classnames': 'classnames',
-    // 'jquery': 'jquery',
-    bootstrap: "bootstrap",
-  },
-
   resolve: {
-    extensions: ["./mjs", ".js", ".jsx", ".scss", ".css", ".json"],
+    extensions: [".js", ".jsx", ".scss", ".css", ".json"],
     alias: {
       jquery: path.join(__dirname, "./jquery-stub.js"),
     },
@@ -76,8 +54,5 @@ module.exports = {
         ],
       },
     ],
-  },
-  performance: {
-    hints: false,
   },
 };
