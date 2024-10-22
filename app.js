03f4331948ed596a736e8762e5844295b7eeed07
codelikesree@gmail.com <codelikesree@gmail.com>
2024-10-22 16:56:23 +0100

    test

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import App from "./home.js";
+// import * as serviceWorker from "./serviceWorker.js";
+// import LoadFormSubmission from "./loadFormSubmission.jsx";
+// import LoadSubmittedForm from "./loadSubmittedForm.jsx";
+
+// ReactDOM.render(
+//   <Router>
+//     <Routes>
+//       <Route path="/create-submission" element={<LoadFormSubmission />} />
+//       <Route path="/preview-submission" element={<LoadSubmittedForm />} />
+//       <Route path="/" element={<App />} />
+//       <Route path="/form" element={<LoadFormSubmission />} />
+//     </Routes>
+//   </Router>,
+//   document.getElementById("form-builder")
+// );
+
+// // Unregister the service worker to avoid caching issues
+// serviceWorker.unregister();
+
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
 import App from "./home.js";
 import * as serviceWorker from "./serviceWorker.js";
 
-import LoadFormSubmission from "./loadFormSubmission.jsx";
-import LoadSubmittedForm from "./loadSubmittedForm.jsx";
-
-ReactDOM.render(
-  <Router>
-    <Routes>
-      {/* Define the /preview route */}
-      <Route path="/create-submission" element={<LoadFormSubmission />} />
-      <Route path="/preview-submission" element={<LoadSubmittedForm />} />
-
-      {/* Define the home route */}
-      <Route path="/" element={<App />} />
-      <Route path="/form" element={<LoadFormSubmission />} />
-    </Routes>
-  </Router>,
-  document.getElementById("form-builder")
-);
+// Render the App component which contains the router
+ReactDOM.render(<App />, document.getElementById("form-builder"));
 
 serviceWorker.unregister();
--- home.js
+++ home.js
@@ -1,25 +1,89 @@
+// import React, { useEffect, useState } from "react";
+// import * as variables from "./variables.js";
+// import DemoBar from "./demobar.js";
+// import FormBuilder from "./src/index.jsx";
+// import { FormBuilderEventKeys } from "./src/utils.js";
+
+// const url = "/";
+// const saveUrl = "/";
+
+// const App = () => {
+//   const [dataForm, setDataForm] = useState([]);
+
+//   useEffect(() => {
+//     window.addEventListener(
+//       "message",
+//       function (event) {
+//         // if (event.origin === "http://localhost:3000") {
+//         if (event.data.key === FormBuilderEventKeys.PostEditForm) {
+//           const formData = event.data.data;
+//           setDataForm(formData);
+//         }
+//         // }
+//       },
+//       false
+//     );
+//   }, [dataForm]);
+
+//   return (
+//     <div
+//       style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+//     >
+//       {" "}
+//       {/* Change to column direction */}
+//       <div style={{ alignItems: "end", width: "70%" }}>
+//         <DemoBar variables={variables} />
+//       </div>
+//       <div style={{ width: "70%", marginTop: "30px" }}>
+//         {!dataForm?.length && (
+//           <FormBuilder.ReactFormBuilder
+//             variables={variables}
+//             url={url}
+//             saveUrl={saveUrl}
+//             locale="en"
+//             saveAlways={false}
+//           />
+//         )}
+//         {!!dataForm?.length && (
+//           <FormBuilder.ReactFormBuilder
+//             variables={variables}
+//             // url={url}
+//             saveUrl={saveUrl}
+//             locale="en"
+//             saveAlways={false}
+//             editMode={true}
+//             edit={true}
+//             data={dataForm}
+//           />
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default App;
 import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import * as variables from "./variables.js";
 import DemoBar from "./demobar.js";
 import FormBuilder from "./src/index.jsx";
 import { FormBuilderEventKeys } from "./src/utils.js";
+import LoadFormSubmission from "./loadFormSubmission.jsx";
+import LoadSubmittedForm from "./loadSubmittedForm.jsx";
 
-const url = "/";
-const saveUrl = "/";
-
-const App = () => {
+// Home component logic (what used to be App's original content)
+const Home = () => {
   const [dataForm, setDataForm] = useState([]);
 
   useEffect(() => {
     window.addEventListener(
       "message",
       function (event) {
-        // if (event.origin === "http://localhost:3000") {
+        // Example code listening to post messages
         if (event.data.key === FormBuilderEventKeys.PostEditForm) {
           const formData = event.data.data;
           setDataForm(formData);
         }
-        // }
       },
       false
     );
@@ -29,8 +93,6 @@ const App = () => {
     <div
       style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
     >
-      {" "}
-      {/* Change to column direction */}
       <div style={{ alignItems: "end", width: "70%" }}>
         <DemoBar variables={variables} />
       </div>
@@ -38,8 +100,8 @@ const App = () => {
         {!dataForm?.length && (
           <FormBuilder.ReactFormBuilder
             variables={variables}
-            url={url}
-            saveUrl={saveUrl}
+            url="/"
+            saveUrl="/"
             locale="en"
             saveAlways={false}
           />
@@ -47,8 +109,7 @@ const App = () => {
         {!!dataForm?.length && (
           <FormBuilder.ReactFormBuilder
             variables={variables}
-            // url={url}
-            saveUrl={saveUrl}
+            saveUrl="/"
             locale="en"
             saveAlways={false}
             editMode={true}
@@ -58,6 +119,23 @@ const App = () => {
         )}
       </div>
     </div>
+  );
+};
+
+// Main App component with routing
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        {/* Define the routes */}
+        <Route path="/create-submission" element={<LoadFormSubmission />} />
+        <Route path="/preview-submission" element={<LoadSubmittedForm />} />
+
+        {/* Define the home route */}
+        <Route path="/" element={<Home />} />
+        <Route path="/form" element={<LoadFormSubmission />} />
+      </Routes>
+    </Router>
   );
 };
 
