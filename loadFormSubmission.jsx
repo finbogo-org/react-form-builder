076a105878538c5cc797a71f6685051a011fb15a
codelikesree@gmail.com <codelikesree@gmail.com>
2024-10-22 14:12:01 +0100

    remove unnecessary dependancy

--- loadFormSubmission.jsx
+++ loadFormSubmission.jsx
@@ -16,7 +16,7 @@ export default function LoadFormSubmission() {
       },
       false
     );
-  }, [questionData]);
+  }, []);
 
   return (
     <>
--- loadSubmittedForm.jsx
+++ loadSubmittedForm.jsx
@@ -16,7 +16,7 @@ export default function LoadSubmittedForm() {
       },
       false
     );
-  }, [data]);
+  }, []);
 
   return (
     <>
