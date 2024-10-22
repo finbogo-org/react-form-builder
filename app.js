603c964a2a4b1b64c161308e0a1300b197219b18
codelikesree@gmail.com <codelikesree@gmail.com>
2024-10-22 18:31:14 +0100

    reveert test

--- app.js
+++ app.js
@@ -1,32 +1,23 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import App from "./home.js";
-// import * as serviceWorker from "./serviceWorker.js";
-// import LoadFormSubmission from "./loadFormSubmission.jsx";
-// import LoadSubmittedForm from "./loadSubmittedForm.jsx";
-
-// ReactDOM.render(
-//   <Router>
-//     <Routes>
-//       <Route path="/create-submission" element={<LoadFormSubmission />} />
-//       <Route path="/preview-submission" element={<LoadSubmittedForm />} />
-//       <Route path="/" element={<App />} />
-//       <Route path="/form" element={<LoadFormSubmission />} />
-//     </Routes>
-//   </Router>,
-//   document.getElementById("form-builder")
-// );
-
-// // Unregister the service worker to avoid caching issues
-// serviceWorker.unregister();
-
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import App from "./home.js";
 import * as serviceWorker from "./serviceWorker.js";
+import LoadFormSubmission from "./loadFormSubmission.jsx";
+import LoadSubmittedForm from "./loadSubmittedForm.jsx";
 
-// Render the App component which contains the router
-ReactDOM.render(<App />, document.getElementById("form-builder"));
+ReactDOM.render(
+  <Router>
+    <Routes>
+      <Route path="/create-submission" element={<LoadFormSubmission />} />
+      <Route path="/preview-submission" element={<LoadSubmittedForm />} />
+      <Route path="/" element={<App />} />
+      <Route path="/test" element={<App />} />
+      <Route path="/form" element={<LoadFormSubmission />} />
+    </Routes>
+  </Router>,
+  document.getElementById("form-builder")
+);
 
+// Unregister the service worker to avoid caching issues
 serviceWorker.unregister();
